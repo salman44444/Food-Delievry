19867e5a62a705b1cc43cf9a01cc7c6219f6cc0b
Salman Qurban <salmanqurban36@gmail.com>
2023-08-16 15:43:24 +0500

    components ui added now starting state management

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import { Fragment, useState } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/cart-provider";
+
+
 function App() {
+
+
+
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+
+    setCartIsShown(true);
+  };
+  const hideCartHandler = () => {
+
+    setCartIsShown(false);
+  }
+
+
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <CartProvider>
+      {cartIsShown && <Cart onHideCart={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+
+    </CartProvider>
+
   );
 }
 
