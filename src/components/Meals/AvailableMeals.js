341b0a9247091b5042a5e0246cf4445f825efc31
Salman Qurban <salmanqurban36@gmail.com>
2023-08-18 13:01:02 +0500

    done phew!

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -30,7 +30,8 @@ const DUMMY_MEALS = [
 ];
 const AvailableMeals = () => {
     const mealsList = DUMMY_MEALS.map(meal => {
-        return (<MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price}></MealItem>)
+        return (
+        <MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price}></MealItem>)
     });
     return (
         <section className={classes.meals}>
